e0e3556a4beed40b1db54a6ec8ff4c62ddcde7d8
FikemiGitHub <fikemiff@gmail.com>
2021-12-01 02:15:02 +0100

    First Commit

---
+++ LibraryApi/Data/SqlCrud.cs
@@ -0,0 +1,36 @@
+﻿using LibraryApi.Models;
+
+namespace LibraryApi.Data
+{
+    public class SqlCrud
+    {
+        private readonly string _connectionString;
+        SqlDataAccess sqlDataAccess = new SqlDataAccess();
+
+        public SqlCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<BookModel> GetAllBooks()
+        {
+            string sqlString = "select Id, Title, Author, Description from dbo.Books order by Id";
+
+            return sqlDataAccess.LoadData(sqlString, _connectionString);
+        }
+
+        public List<BookModel> GetBookById(int id)
+        {
+            string sqlString = "select Id, Title, Author, Description from dbo.Books where id = @id";
+
+            return sqlDataAccess.LoadDataById(id, sqlString, _connectionString);
+        }
+
+        public async Task<List<BookModel>> PostNewBook(BookModel newBook)
+        {
+            string sqlProcedure = "InsertNewBook";
+
+            return await Task.FromResult(sqlDataAccess.PostNewBook(newBook, sqlProcedure, _connectionString));
+        }
+    }
+}
---
+++ LibraryApi/Models/BookModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApi.Models
+{
+    public class BookModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
